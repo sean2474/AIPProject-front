b6f2651e6449197beb5c958ba4a7265d509c85fb
sean2474 <47488868+sean2474@users.noreply.github.com>
2023-08-18 21:31:52 +0900

    fixed home page overflow in android

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -2,6 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:front/data/data.dart';
 import 'package:front/data/shared_preference/save_preference.dart';
+import 'package:front/widgets/assets.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import 'setting_modal.dart';
 
@@ -13,75 +15,89 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  final RefreshController _refreshController = RefreshController(initialRefresh: false);
+
   static void showSetting(BuildContext context) {
-    
-  showModalBottomSheet(
-    context: context,
-    isScrollControlled: true,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(15.0)),
-    ),
-    builder: (BuildContext context) {
-      return SettingModal();
-    },
-  );
-}
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(15.0)),
+      ),
+      builder: (BuildContext context) {
+        return SettingModal();
+      },
+    );
+  }
   
   @override
   Widget build(BuildContext context) {
     ColorScheme colorScheme = Theme.of(context).colorScheme;
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            width: double.infinity,
-            height: 200,
-            margin: EdgeInsets.only(top: 30),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: CachedNetworkImage(
-                imageUrl: "${Data.apiService.baseUrl}/data/daily-schedule/image/2023-07-28",
-                errorWidget: (context, url, error) => Container(
-                  child: Image(
-                    image: AssetImage('assets/icons/imageNotFound.png'),
+    Assets assets = Assets(currentPage: HomePage());
+    return SmartRefresher(
+      enablePullDown: true,
+      enablePullUp: false,
+      controller: _refreshController,
+      header: assets.refreshHeader(indicatorColor: Colors.grey,),
+      onRefresh: () => Future.delayed(const Duration(milliseconds: 500), () async {
+        setState(() {});
+        _refreshController.refreshCompleted();
+      }),
+      child: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 200,
+                margin: EdgeInsets.only(top: 30),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: CachedNetworkImage(
+                    imageUrl: "${Data.apiService.baseUrl}/data/daily-schedule/image/2023-07-28",
+                    errorWidget: (context, url, error) => Container(
+                      child: Image(
+                        image: AssetImage('assets/icons/imageNotFound.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                     fit: BoxFit.cover,
                   ),
                 ),
-                fit: BoxFit.cover,
               ),
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height / 2.3,
-            margin: EdgeInsets.only(top: 40),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: colorScheme.onBackground, width: 1)
-            ),
-            child: Column( 
-              children: [
-                 // check sent to GET: /auth/testToken with UID in header["uid"]
-                TextButton(
-                  onPressed: () async => Data.apiService.checkUIDAuth(), 
-                  child: Text("Check UID")
+              Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height / 2.3,
+                margin: EdgeInsets.only(top: 40),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: colorScheme.onBackground, width: 1)
                 ),
-                TextButton(
-                  onPressed: () async => Save.all(),
-                  child: Text("save settings")
+                child: Column( 
+                  children: [
+                     // check sent to GET: /auth/testToken with UID in header["uid"]
+                    TextButton(
+                      onPressed: () async => Data.apiService.checkUIDAuth(), 
+                      child: Text("Check UID")
+                    ),
+                    TextButton(
+                      onPressed: () async => Save.all(),
+                      child: Text("save settings")
+                    ),
+                    TextButton(
+                      onPressed: () async => Save.reset(),
+                      child: Text("reset")
+                    ),
+                  ],
                 ),
-                TextButton(
-                  onPressed: () async => Save.reset(),
-                  child: Text("reset")
-                ),
-              ],
-            ),
+              )
+            ],
           )
-        ],
-      )
+        ),
+      ),
     );
   }
 }
